fe4c4a748bb5904a0005771e7ab2e382b880a979
Ethan Dyas <41522493+ethan-dyas438@users.noreply.github.com>
2018-08-13 18:48:57 -0500

    Added comment about save function.

--- SimpleStory/main.cpp
+++ SimpleStory/main.cpp
@@ -66,7 +66,7 @@ int main()
 void save(string const name) {
     ofstream mysave;
     mysave.open("Save.txt");
-    mysave << name;
+    mysave << name; //save name string to the text file
     mysave.close(); // Closes save function
 
 }
